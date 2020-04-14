372d0e650ce552fb13c169de102bd360a4a2dde0
manuelnelson <elnels@gmail.com>
2020-04-13 21:10:14 -0400

    deploy

---
+++ precache-manifest.33a917d8f6429c07691081259cf2dece.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca1b92b9cada932c9561",
+    "url": "/css/app.00475722.css"
+  },
+  {
+    "revision": "ca1b92b9cada932c9561",
+    "url": "/js/app.61ccdbd7.js"
+  },
+  {
+    "revision": "b440a1178a0cfe1c5a5e",
+    "url": "/js/chunk-vendors.407edd8e.js"
+  },
+  {
+    "revision": "b665046e989217465d72",
+    "url": "/css/home.89494a28.css"
+  },
+  {
+    "revision": "b665046e989217465d72",
+    "url": "/js/home.c8e9b1db.js"
+  },
+  {
+    "revision": "9d65767ab73ea917ce328ba7e9cf0b97",
+    "url": "/img/black-and-white.9d65767a.jpg"
+  },
+  {
+    "revision": "f59e18948219a08ad53bf4558120afa2",
+    "url": "/img/onenorth.f59e1894.png"
+  },
+  {
+    "revision": "5e4ec0a6a3edab57c943a68ca12b21cc",
+    "url": "/img/burnsmcd.5e4ec0a6.png"
+  },
+  {
+    "revision": "fe3acd27050c71c53a09389670da6580",
+    "url": "/img/youth-indicators-2.fe3acd27.png"
+  },
+  {
+    "revision": "02c9899792a5c8912ed3e3d8cb92ee40",
+    "url": "/img/api-highways.02c98997.png"
+  },
+  {
+    "revision": "404744bf7e53bddff7da33228cd0cf64",
+    "url": "/img/api-highways-2.404744bf.png"
+  },
+  {
+    "revision": "4c22ca1262f6751a56af83c67c3e6a2f",
+    "url": "/img/youth-indicators.4c22ca12.png"
+  },
+  {
+    "revision": "dfc03b686527e98ff2ec5bb1960ed81d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
